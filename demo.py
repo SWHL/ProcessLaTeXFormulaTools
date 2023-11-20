260f16bc25f6d7468a6b63247219ba442ec955ea
SWHL <liekkaskono@163.com>
2023-11-20 14:38:29 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 # @Author: SWHL
 # @Contact: liekkaskono@163.com
-from process_formula_t import NormalizeFormula
+from process_formula import NormalizeFormula
 
 normlizer = NormalizeFormula()
 
